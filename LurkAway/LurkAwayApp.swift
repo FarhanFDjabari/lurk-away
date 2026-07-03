5205e4dd8fa63eec6efe119b8c8ea8032236bbf5
FarhanFDjabari <djabari.dev@gmail.com>
2026-07-04 05:05:32 +0700

    Initial Commit

---
+++ LurkAway/LurkAwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  LurkAwayApp.swift
+//  LurkAway
+//
+//  Created by Djabari on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct LurkAwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
